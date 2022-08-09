608ff799fba9bb7fb27474bfe64242c45a727230
FakhriAladhami <105823355+FakhriAladhami@users.noreply.github.com>
2022-08-09 08:25:09 -0500

    Add files via upload

---
+++ Fakhri3.js
@@ -0,0 +1,11 @@
+function changebar(){
+    var navbar= document.getElementById('navbar')
+    var barvalue= window.scrollY
+if(navbar < 150){
+    navbar.classList.remove('bgcolor')
+}else{
+    navbar.classList.add('bgcolor')
+}
+}
+
+window.addEventListener('scroll',changebar)
