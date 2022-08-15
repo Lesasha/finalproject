9e4248946994eac4eb93661ea2eeea131e0de305
FakhriAladhami <105823355+FakhriAladhami@users.noreply.github.com>
2022-08-15 08:38:35 -0500

    Add files via upload

---
+++ Fakhri33.js
@@ -0,0 +1,13 @@
+
+   $(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+         $('nav').addClass('bgcolor');
+    }
+
+    else {
+          $('nav').removeClass('bgcolor');
+    }
+})
+
+
+
