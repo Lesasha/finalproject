a7e631d3263cc248f930702280e61139981f853b
Grammy2201 <gregorys234@gmail.com>
2022-09-09 16:36:14 -0500

    changes made

--- eventsGS.js
+++ eventsGS.js
@@ -7,6 +7,34 @@ $(window).on("scroll", function() {
   }
 })
 
+google.maps.event.addDomListener(window, 'load', function() {
+  var places = new google.maps.places.Autocomplete(document
+          .getElementById('txtPlaces'));
+  google.maps.event.addListener(places, 'place_changed', function() {
+      var place = places.getPlace();
+      var address = place.formatted_address;
+      var  value = address.split(",");
+      count=value.length;
+      country=value[count-1];
+      state=value[count-2];
+      city=value[count-3];
+      var z=state.split(" ");
+      document.getElementById("selCountry").text = country;
+      var i =z.length;
+      document.getElementById("pstate").value = z[1];
+      if(i>2)
+      document.getElementById("pzcode").value = z[2];
+      document.getElementById("pCity").value = city;
+      var latitude = place.geometry.location.lat();
+      var longitude = place.geometry.location.lng();
+      var mesg = address;
+      document.getElementById("txtPlaces").value = mesg;
+      var lati = latitude;
+      document.getElementById("plati").value = lati;
+      var longi = longitude;
+      document.getElementById("plongi").value = longi;            
+  });
+});
 // // Get the modal
 // //var modal = document.getElementById("myModal");
 
